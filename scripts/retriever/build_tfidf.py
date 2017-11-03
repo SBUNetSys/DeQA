087651c75203dd5de151881d51f73b7499acfb71
Arron Cao <qqcao.cs@gmail.com>
2017-11-03 13:14:39 -0400

    fix: build tfidf model save path

--- scripts/retriever/build_tfidf.py
+++ scripts/retriever/build_tfidf.py
@@ -189,10 +189,7 @@ if __name__ == '__main__':
     logger.info('Getting word-doc frequencies...')
     freqs = get_doc_freqs(count_matrix)
 
-    basename = os.path.splitext(os.path.basename(args.db_path))[0]
-    basename += ('-tfidf-ngram=%d-hash=%d-tokenizer=%s' %
-                 (args.ngram, args.hash_size, args.tokenizer))
-    filename = os.path.join(args.out_dir, basename)
+    filename = os.path.join(args.out_dir, retriever.DEFAULTS['tfidf_path'])
 
     logger.info('Saving to %s.npz' % filename)
     metadata = {
