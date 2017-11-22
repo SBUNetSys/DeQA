5d23d2286e36b61f8a7b9cb16888c8151ecb8f56
Arron Cao <qqcao.cs@gmail.com>
2017-11-22 17:29:14 -0500

    fix: paragraphs and words counting

--- stats/count_para_words.py
+++ stats/count_para_words.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-import json
 import argparse
 import ast
+from extract_util import extract_lines
 
 SEPARATOR_STR = 'question: '
-END_STR = ' ]'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_file', type=str, default='logs/trec_docs_5.log')
+    parser.add_argument('-l', '--log_file', type=str, default='logs/trec_docs_5.log')
 
     args = parser.parse_args()
-
-    i = 0
-
-    for line in open(args.log_file, encoding="utf-8"):
-        if SEPARATOR_STR in line:
-            question_line = line[line.index(SEPARATOR_STR)+len(SEPARATOR_STR):line.index(END_STR)]
-
+    for i, question_line in enumerate(extract_lines(args.log_file, SEPARATOR_STR)):
             question, para_words_strings = question_line.split('paragraphs: ')
             para_words_list = ast.literal_eval(para_words_strings)
             print('%d,' % (i + 1), 'paragraphs: %d ' % len(para_words_list),
                   'words: %d ' % sum(para_words_list), 'question: %s' % question.strip())
-            i += 1
