274916e8c122ec239711a9693f038b7b027de4ec
Arron Cao <qqcao.cs@gmail.com>
2017-11-22 17:40:45 -0500

    fix: extraction format

--- stats/count_para_words.py
+++ stats/count_para_words.py
@@ -3,14 +3,13 @@ import argparse
 import ast
 from extract_util import extract_lines
 
-SEPARATOR_STR = 'question: '
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log_file', type=str, default='logs/trec_docs_5.log')
+    parser.add_argument('-l', '--log_file', type=str, default='logs/pc/trec_docs_5.log')
 
     args = parser.parse_args()
-    for i, question_line in enumerate(extract_lines(args.log_file, SEPARATOR_STR)):
+    for i, question_line in enumerate(extract_lines(args.log_file, 'question_p: ', ' ]')):
             question, para_words_strings = question_line.split('paragraphs: ')
             para_words_list = ast.literal_eval(para_words_strings)
             print('%d,' % (i + 1), 'paragraphs: %d ' % len(para_words_list),
