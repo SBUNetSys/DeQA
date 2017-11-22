d0127f01803c8cfdcd9c917125ed91a898fc3b85
Arron Cao <qqcao.cs@gmail.com>
2017-11-22 17:39:04 -0500

    fix: extraction format

--- stats/extract_util.py
+++ stats/extract_util.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-END_STR = ' ]'
 
-
-def extract_lines(log_file_, flag_words):
+def extract_lines(log_file_, flag_words, end_flag=None):
     lines_ = []
     for line_ in open(log_file_, encoding="utf-8"):
         if flag_words in line_:
-            target_line = line_[line_.index(flag_words) + len(flag_words):line_.index(END_STR)]
+            end_index = line_.index(end_flag) if end_flag else None
+            target_line = line_[line_.index(flag_words) + len(flag_words):end_index]
             lines_.append(target_line)
     return lines_
