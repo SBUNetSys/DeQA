3860dd7373be25a820628f1d97d330bdd917feb2
Arron Cao <qqcao.cs@gmail.com>
2017-12-05 17:29:51 -0500

    add: time extraction for reader

--- stats/extract_time.py
+++ stats/extract_time.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 import argparse
+import ast
 from extract_util import extract_lines
 
 ENCODING = "utf-8"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log_file', type=str, default='logs/tx2/trec_paras_100.log')
+    parser.add_argument('-l', '--log_file', type=str, default='logs/trec_paras_150.log')
 
     args = parser.parse_args()
 
     # time_flags = ['docs retrieved', 'paragraphs predicted', 'queries processed']
-    time_flags = ['docs retrieved', 'paragraphs predicted', 'queries processed']
+    time_flags = ['docs retrieved', 'paragraphs predicted', 'queries processed',
+                  'embedding lookup', 'weighted question attention emb', 'doc_rnn',
+                  'question_rnn', 'question_self_attn', 'question_weighted_avg', 'start_attn', 'end_attn']
 
     stage_times = [list() for _ in time_flags]
 
@@ -19,6 +22,20 @@ if __name__ == '__main__':
         for line in extract_lines(args.log_file, '%s [time]: ' % time_flag, ' s ]'):
             stage_time.append(float(line))
 
-    for stage_time in stage_times:
+    for stage_time in stage_times[:3]:
         avg_time = sum(stage_time) / len(stage_time)
         print('%.4f' % avg_time)
+    for stage_time in stage_times[3:]:
+        avg_time = sum(stage_time) * 150 / len(stage_time)
+        print('%.4f' % avg_time)
+
+    # query_doc_dict = {}
+    # for line in extract_lines(args.log_file, 'question_d:', ' ]'):
+    #     question, sec_strings = line.split(', query:')
+    #     start_index = sec_strings.index('doc_ids:') + len('doc_ids:')
+    #     end_index = sec_strings.index(', doc_scores:')
+    #     doc_id_strings = sec_strings[start_index:end_index]
+    #     top_doc_ids = ast.literal_eval(doc_id_strings)
+    #     query_doc_dict[question.strip()] = top_doc_ids
+    #     if len(top_doc_ids) != 150:
+    #         print(question, len(top_doc_ids))
