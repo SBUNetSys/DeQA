68b0da28748c46534a6e68afc73040045e150fa5
Arron Cao <qqcao.cs@gmail.com>
2017-12-14 15:34:58 -0500

    fix: improve extracting performance

--- stats/extract_util.py
+++ stats/extract_util.py
@@ -2,10 +2,8 @@
 
 
 def extract_lines(log_file_, flag_words, end_flag=None):
-    lines_ = []
     for line_ in open(log_file_, encoding="utf-8"):
         if flag_words in line_:
             end_index = line_.index(end_flag) if end_flag else None
             target_line = line_[line_.index(flag_words) + len(flag_words):end_index]
-            lines_.append(target_line)
-    return lines_
+            yield target_line
