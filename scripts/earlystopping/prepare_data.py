562c6dc4fae53d2cf27271c61511353895fc6a9e
Arron Cao <qqcao.cs@gmail.com>
2017-12-15 20:56:06 -0500

    fix: add question feature to data records

--- scripts/earlystopping/prepare_data.py
+++ scripts/earlystopping/prepare_data.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from drqa.retriever.utils import normalize
 from drqa.pipeline import DEFAULTS
 from drqa.reader.utils import exact_match_score, metric_max_over_ground_truths
+from drqa.reader.utils import slugify
 ENCODING = "utf-8"
 
 if __name__ == '__main__':
@@ -27,28 +28,40 @@ if __name__ == '__main__':
                                               open(prediction_file, encoding=ENCODING)):
             data = json.loads(data_line)
             question = data['question']
+            q_id = slugify(question)
+            q_path = os.path.join(DEFAULTS['features'], '%s.json' % q_id)
+            q_feature = None
+            if os.path.exists(q_path):
+                q_data = open(q_path, encoding=ENCODING).read()
+                q_feature = json.loads(q_data)
+            else:
+                print('%s not exist!' % q_path)
+                exit(-1)
+                doc_missing_count += 1
             answer = [normalize(a) for a in data['answer']]
             prediction = json.loads(prediction_line)
             for entry in prediction:
                 doc_id = entry['doc_id']
                 record = OrderedDict()
                 record['q'] = question
-                record['d_id'] = doc_id
                 record['a'] = normalize(entry['span'])
                 record['a_s'] = entry['span_score']
                 record['d_s'] = entry['doc_score']
                 exact_match = metric_max_over_ground_truths(exact_match_score, normalize(entry['span']), answer)
                 record['stop'] = 1 if exact_match else 0
 
+                record['q_idx'] = q_feature['idx']
+                record['q_tf'] = q_feature['tf']
+                record['q_ner'] = q_feature['ner']
+                record['q_pos'] = q_feature['pos']
                 doc_path = os.path.join(DEFAULTS['features'], '%s.json' % doc_id)
                 if os.path.exists(doc_path):
                     doc_data = open(doc_path, encoding=ENCODING).read()
                     feature = json.loads(doc_data)
-
-                    record['d_l'] = feature['l_p']
-                    record['tf'] = feature['tf']
-                    record['ner'] = feature['ner']
-                    record['pos'] = feature['pos']
+                    record['p_idx'] = feature['idx']
+                    record['p_tf'] = feature['tf']
+                    record['p_ner'] = feature['ner']
+                    record['p_pos'] = feature['pos']
                 else:
                     print('%s not exist!' % doc_path)
                     doc_missing_count += 1
