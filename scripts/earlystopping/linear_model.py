8b9bcf1e91a0d70889030d716b2fa899233476e5
Arron Cao <qqcao.cs@gmail.com>
2017-12-19 16:30:26 -0500

    fix: logger timestamp

--- scripts/earlystopping/linear_model.py
+++ scripts/earlystopping/linear_model.py
@@ -365,8 +365,7 @@ if __name__ == '__main__':
         torch.cuda.manual_seed(args.random_seed)
     # Set logging
     logger.setLevel(logging.INFO)
-    fmt = logging.Formatter('%(asctime)s: [ %(message)s ]',
-                            '%m/%d/%Y %I:%M:%S %p')
+    fmt = logging.Formatter('%(asctime)s.%(msecs)d: [ %(message)s ]', '%m/%d/%Y_%H:%M:%S')
     console = logging.StreamHandler()
     console.setFormatter(fmt)
     logger.addHandler(console)
