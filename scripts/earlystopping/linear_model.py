190d21a5dcd9a559aaa643a031015e080a2ded36
Arron Cao <qqcao.cs@gmail.com>
2017-12-18 20:36:47 -0500

    add: data vectorization

--- scripts/earlystopping/linear_model.py
+++ scripts/earlystopping/linear_model.py
@@ -45,13 +45,16 @@ def batchify(batch_):
 
 class RecordDataset(Dataset):
 
-    def __init__(self, records_, has_answer=False):
+    def __init__(self, records_, weight_file_, has_answer=False):
         self.records_ = records_
         self.has_answer = has_answer
-        # logger.info('Loading model %s' % weights_file)
-        # saved_params = torch.load(weights_file, map_location=lambda storage, loc: storage)
-        # self.word_dict = saved_params['word_dict']
-        # self.state_dict = saved_params['state_dict']
+        logger.info('Loading model %s' % weight_file_)
+        saved_params = torch.load(weight_file_, map_location=lambda storage, loc: storage)
+        state_dict = saved_params['state_dict']
+        emb_weights = state_dict['embedding.weight']
+        self.embedding = nn.Embedding(emb_weights.size(0), emb_weights.size(1), padding_idx=0)
+        self.embedding.weight = nn.Parameter(emb_weights)
+        self.embedding.weight.requires_grad = False
 
     def __len__(self):
         return len(self.records_)
@@ -60,28 +63,80 @@ class RecordDataset(Dataset):
         return self.vectorize(self.records_[index],  self.has_answer)
 
     def vectorize(self, record_, has_answer=False):
-        answer = record_['a']
-        answer_score = record_['a_s']
-        doc_score = record_['d_s']
-        question = record_['q']
-        ner = record_['ner']
-        pos = record_['pos']
-        tf = record_['tf']
+        a_s = record_['a_s']
+        a_s_t = torch.FloatTensor([a_s])
+
+        d_s = record_['d_s']
+        d_s_t = torch.FloatTensor([d_s])
+
+        a_idx = record_['a_idx']
+        a_idx_t = torch.LongTensor(a_idx)
+        a_emb = self.embedding(a_idx_t)
+
+        p_ner = record_['p_ner']
+        p_ner_t = torch.zeros(len(p_ner), len(Tokenizer.NER))
+        for i, w in enumerate(p_ner):
+            if w in Tokenizer.NER_DICT:
+                p_ner_t[i][Tokenizer.NER_DICT[w]] = 1.0
+
+        p_pos = record_['p_pos']
+        p_pos_t = torch.zeros(len(p_pos), len(Tokenizer.POS))
+        for i, w in enumerate(p_pos):
+            if w in Tokenizer.POS_DICT:
+                p_pos_t[i][Tokenizer.POS_DICT[w]] = 1.0
+
+        p_tf = record_['p_tf']
+        p_tf_t = torch.FloatTensor(p_tf)
+
+        p_idx = record_['p_idx']
+        p_idx_t = torch.LongTensor(p_idx)
+        p_emb = self.embedding(p_idx_t)
+
+        p_f = torch.cat([p_emb, p_ner_t, p_pos_t, p_tf_t], dim=2)
+        q_ner = record_['q_ner']
+        q_ner_t = torch.zeros(len(q_ner), len(Tokenizer.NER))
+        for i, w in enumerate(q_ner):
+            if w in Tokenizer.NER_DICT:
+                q_ner_t[i][Tokenizer.NER_DICT[w]] = 1.0
+
+        q_pos = record_['q_pos']
+        q_pos_t = torch.zeros(len(q_pos), len(Tokenizer.POS))
+        for i, w in enumerate(q_pos):
+            if w in Tokenizer.POS_DICT:
+                q_pos_t[i][Tokenizer.POS_DICT[w]] = 1.0
+
+        q_tf = record_['q_tf']
+        q_tf_t = torch.FloatTensor(q_tf)
+
+        q_idx = record_['q_idx']
+        q_idx_t = torch.LongTensor(q_idx)
+        q_emb = self.embedding(q_idx_t)
+        q_f = torch.cat([q_emb, q_ner_t, q_pos_t, q_tf_t], dim=2)
+
         if has_answer:
             label = record_['stop']
+            l_t = torch.FloatTensor([label])
+            return a_s_t, d_s_t, a_emb, q_f, p_f, l_t
+        else:
+            return a_s_t, d_s_t, a_emb, q_f, p_f
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--record_file',
                         default='../../data/earlystopping/records-10.txt')
+    parser.add_argument('-w', '--weight_file',
+                        default='../../data/reader/multitask.mdl')
 
     args = parser.parse_args()
     NUM_LABELS = 2
 
+    weight_file = args.weight_file
+
     record_file = args.record_file
     for data_line in open(record_file, encoding=ENCODING):
-        data = json.loads(data_line)
+        record_ = json.loads(data_line)
+        print(record_)
 
     # # Define model
     # fc = torch.nn.Linear(W_target.size(0), 1)
