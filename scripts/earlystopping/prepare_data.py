19556a1c306347b585364c939120226cfecea022
Arron Cao <qqcao.cs@gmail.com>
2018-01-05 18:22:26 -0500

    add: label judgement for data records

--- scripts/earlystopping/prepare_data.py
+++ scripts/earlystopping/prepare_data.py
@@ -7,21 +7,20 @@ from drqa.retriever.utils import normalize
 from drqa.pipeline import DEFAULTS
 from drqa.reader.utils import exact_match_score, metric_max_over_ground_truths
 from drqa.reader.utils import slugify
-
 ENCODING = "utf-8"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--prediction_file',
-                        default='../../data/earlystopping/SQuAD-v1.1-dev-10-multitask-pipeline.preds')
-    parser.add_argument('-a', '--answer_file', default='../../data/datasets/SQuAD-v1.1-dev-10.txt')
+                        default='../../data/earlystopping/SQuAD-v1.1-dev-100-multitask-pipeline.preds')
+    parser.add_argument('-a', '--answer_file', default='../../data/datasets/SQuAD-v1.1-dev-100.txt')
     parser.add_argument('-r', '--record_file',
-                        default='../../data/earlystopping/records-10.pkl')
+                        default='../../data/earlystopping/records-100.txt')
     args = parser.parse_args()
 
     doc_missing_count = 0
     no = 0
-    record_path = os.path.splitext(args.record_file)[0] + '.txt'
+    record_path = args.record_file
     answer_file = args.answer_file
     prediction_file = args.prediction_file
     max_q_len = 0
@@ -43,21 +42,28 @@ if __name__ == '__main__':
                 doc_missing_count += 1
             answer = [normalize(a) for a in data['answer']]
             prediction = json.loads(prediction_line)
-            for entry in prediction:
+            ranked_prediction = sorted(prediction, key=lambda k: k['doc_score'])
+            found_correct = False
+            for entry in ranked_prediction:
                 doc_id = entry['doc_id']
                 record = OrderedDict()
                 record['q'] = question
                 record['a'] = normalize(entry['span'])
                 record['a_s'] = entry['span_score']
                 record['d_s'] = entry['doc_score']
-                exact_match = metric_max_over_ground_truths(exact_match_score, normalize(entry['span']), answer)
-                record['stop'] = 1 if exact_match else 0
+                if found_correct:
+                    record['stop'] = 1
+                else:
+                    exact_match = metric_max_over_ground_truths(exact_match_score, normalize(entry['span']), answer)
+                    if exact_match:
+                        found_correct = True
+                        record['stop'] = 1
+                    else:
+                        record['stop'] = 0
 
-                record['q_idx'] = q_feature['idx']
                 if len(q_feature['idx']) > max_q_len:
                     max_q_len = len(q_feature['idx'])
 
-                record['q_tf'] = q_feature['tf']
                 record['q_ner'] = q_feature['ner']
                 record['q_pos'] = q_feature['pos']
 
