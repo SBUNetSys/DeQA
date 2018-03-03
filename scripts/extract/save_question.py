963eaca13ee48712527ea86749c580a53e591d00
Arron Cao <qqcao.cs@gmail.com>
2018-03-03 15:59:17 -0500

    add: question mapping

--- scripts/extract/save_question.py
+++ scripts/extract/save_question.py
@@ -1,16 +1,42 @@
 import json
 import argparse
+import os
+from drqa import tokenizers
+from drqa.retriever import utils
+from drqa.reader.utils import slugify
+
+
+def gen_query(question_):
+    normalized = utils.normalize(question_)
+    tokenizer = tokenizers.get_class('simple')()
+    tokens = tokenizer.tokenize(normalized)
+    words = tokens.ngrams(n=1, uncased=True, filter_fn=utils.filter_ngram)
+    query_ = ' '.join(words)
+    return query_
+
+
+def gen_name(question_):
+    return slugify(question_)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('answer_file', type=str)
-    parser.add_argument('-s', '--save_file', type=str, default='out.txt')
+    parser.add_argument('data_file', type=str)
 
     args = parser.parse_args()
-    answer_file = args.answer_file
+    data_file = args.data_file
+    data_file_name, ext = os.path.splitext(data_file)
+    question_file = data_file_name + '.question' + ext
+    query_file = data_file_name + '.query' + ext
+    name_file = data_file_name + '.name' + ext
 
-    with open(args.save_file, 'w') as f:
-        for data_line in open(answer_file):
+    with open(question_file, 'w') as fq, open(query_file, 'w') as fy, open(name_file, 'w') as fn:
+        for data_line in open(data_file):
             data = json.loads(data_line)
             question = data['question']
-            f.write(question + '\n')
+            fq.write(question + '\n')
+            query = gen_query(question)
+            fy.write(query + '\n')
+            name = gen_name(question)
+            fn.write(name + '\n')
+
